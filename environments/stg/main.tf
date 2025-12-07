12c9b0917a4c5cbbc44eea28af5018b1b02b4f25
Kohei Nagase <k.nagase.biz@gmail.com>
2025-12-07 13:37:57 +0900

    initial commit

---
+++ environments/stg/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = "~> 1.13.4"
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
+  backend "local" {}
+}
+
+resource "null_resource" "hello_stg_exec" {
+  provisioner "local-exec" {
+    command = "echo Stg"
+  }
+
+  # triggers を使うと毎回実行されるサンプルになります
+  triggers = {
+    always_run = timestamp()
+  }
+}
